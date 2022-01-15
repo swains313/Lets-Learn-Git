d7ebfa0acfa862ef07b5ea7b377ada17d6f3f562
saumyaranjan swain <swains313@gmail.com>
2022-01-15 19:24:16 +0530

    adding message Sum.java

--- Sum.java
+++ Sum.java
@@ -1,7 +1,7 @@
 public class Sum{
     public static void main(String[]args) {
         int a=5,b=10;
-        System.out.println(a+b);
+        System.out.println(a+"The sum is" +b);
 
         
     }
