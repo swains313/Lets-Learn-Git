1bc682d4e2a91ccfaa675df530bc704ab2e26f82
saumyaranjan swain <swains313@gmail.com>
2023-02-16 17:22:07 +0530

    initial commit

--- Sum.java
+++ Sum.java
@@ -1,9 +1,9 @@
-public class Sum{
-    public static void main(String[]args) {
-        int a=5,b=10;
-        System.out.println(a+"The sum is" +b);
+public class Sum {
 
-        
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        System.out.println("a+b");
     }
-
+    
 }
